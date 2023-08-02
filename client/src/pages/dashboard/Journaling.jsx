ce332fe8d60d69a8185503b434ebc18352c680dd
Bahar812 <104080956+Bahar812@users.noreply.github.com>
2023-08-02 22:23:28 +0700

    frontend journaling dashboard

--- client/src/pages/dashboard/Journaling.jsx
+++ client/src/pages/dashboard/Journaling.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Journaling = () => {
   return (
-    <div>Journaling</div>
+    <div className="flex flex-col items-start justify-center w-full p-8 gap-4">
+      <div className= "flex flex-col w-full items-start justify-start">
+        <p className="font-sans font-semibold text-brown text-4x1">
+          Thursday
+        </p>
+      </div>
+    </div>
   )
 }
 
