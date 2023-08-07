8c8ca82b5dfd5a241de86f8877588bdcaf08d394
Bahar812 <104080956+Bahar812@users.noreply.github.com>
2023-08-08 03:59:19 +0700

    Meditation

---
+++ client/src/components/NavbarMeditation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavbarMeditation() {
+    return (
+        <div className='h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl'>
+            <i className='fa fa-spotify mr-5'></i>
+            Mindflow
+        </div>
+    )
+}
+
+
